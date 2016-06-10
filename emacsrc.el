7c45185a9a151c190b696accc97d9a60b8aff8a6
Markus Bayer <bayer_markus@web.de>
2016-06-10 10:37:13 +0200

    added structure, whitespace highlighting

--- emacsrc.el
+++ emacsrc.el
@@ -1,58 +1,67 @@
-;;; activate package managers
-(require 'package)
+;;; general {
+    ;;; theme
+    (load-theme 'misterioso)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+    ;;; disable blinking cursor
+    (blink-cursor-mode 0)
 
-(setq package-enable-at-startup nil)
-(package-initialize)
+    ;;; disable welcome window
+    (setq ns-pop-up-frames nil)
 
-;;; org mode
+    ;;; disable audio bell
+     (setq visible-bell t)
 
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(require 'org)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+    ;;; disable tool-bar
+    (tool-bar-mode -1)
+    ;;; set font size to 16pt
+    (set-face-attribute 'default nil :height 160)
 
-;;; os-rst (org mode rst export)
-(require 'ox-rst)
+    ;;; show trailing whitespace
+    (setq-default show-trailing-whitespace t)
+;;; }
 
-;;; evil
-(require 'evil)
-(evil-mode 1)
+;;; key mappings {
+    ;;; map command key to meta
+    (if (boundp 'ns-command-modifier)
+        (setq ns-command-modifier 'meta))
+    (if (boundp 'ns-option-modifier)
+        (setq ns-option-modifier nil))
 
-;;; helm
-(require 'helm-config)
+    ;;; osx: quit, close, copy, paste, save
+    (global-set-key (kbd "M-q") 'save-buffers-kill-terminal)
+    (global-set-key (kbd "M-w") 'kill-buffer)
+    (global-set-key (kbd "M-c") 'kill-ring-save)
+    (global-set-key (kbd "M-v") 'yank)
+    (global-set-key (kbd "M-s") 'save-buffer)
+;;; }
 
-;;; fireplace
-(require 'fireplace)
 
-;;; map command key to meta
-(if (boundp 'ns-command-modifier)
-    (setq ns-command-modifier 'meta))
+;;; plugins {
+    ;;; activate package managers
+    (require 'package)
+    (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+    (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+    (setq package-enable-at-startup nil)
+    (package-initialize)
 
-(if (boundp 'ns-option-modifier)
-    (setq ns-option-modifier nil))
+    ;;; org-mode
+    (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+    (require 'org)
+    (define-key global-map "\C-cl" 'org-store-link)
+    (define-key global-map "\C-ca" 'org-agenda)
+    (setq org-log-done t)
 
-;;; disable blinking cursor
-(blink-cursor-mode 0)
+    ;;; os-rst (org-mode rst export)
+    (require 'ox-rst)
 
-;;; disable welcome window
-(setq ns-pop-up-frames nil)
+    ;;; evil
+    (require 'evil)
+    (evil-mode 1)
 
-;;; disable audio bell
- (setq visible-bell t)
+    ;;; helm
+    (require 'helm-config)
 
-;;; disable tool-bar
-(tool-bar-mode -1)
-;;; set font size to 16pt
-(set-face-attribute 'default nil :height 160)
-
-;;; theme
-(load-theme 'misterioso)
-
-;;; key mappings
-(global-set-key (kbd "M-q") 'save-buffers-kill-terminal)
-(global-set-key (kbd "M-w") 'kill-buffer)
+    ;;; fireplace
+    (require 'fireplace)
+;;; }
