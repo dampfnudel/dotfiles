b96247f8bea1a68db78c535aadc319d6e7c68d27
Markus Bayer <bayer_markus@web.de>
2016-10-09 14:12:48 +0200

    added plugins: company, expand-region, git-gutter. helm-projectile, projectile, restclient, tabbar, undo-tree, yasnippet

--- emacsrc.el
+++ emacsrc.el
@@ -10,12 +10,9 @@
 ; ASCII-Art credits: http://patorjk.com/software/taag/#p=testall&h=0&v=0&f=ANSI%20Shadow&t=.emacsrc
 
 ;;; TODO {
-    ; osx
-        ; shell open
-        ; copy
-        ; cmd a
+    ; modeline
+    ; errors/warnings
     ; orgmodify
-        ; cheatsheets
         ; org-files
         ; archive
         ; capture
@@ -23,15 +20,16 @@
     ; plugins
         ; magit
         ; restclient
-        ; dired
-        ; ace-jump
-        ; yasnippet
-        ; undo tree
-        ; expand region
         ; eshell
         ; paredit
         ; doctor
         ; dunnet
+        ;https://github.com/Fuco1/smartparens
+        ;https://github.com/auto-complete/auto-complete
+        ;https://github.com/abo-abo/avy
+        ;https://github.com/nonsequitur/smex/
+        ;https://github.com/jacktasia/dumb-jump
+        ;https://github.com/abo-abo/hydra
 ;;; }
 
 ;;; general {
@@ -82,6 +80,10 @@
         (define-key map (kbd "M-v") 'yank)                              ; paste
         (define-key map (kbd "M-s") 'save-buffer)                       ; save
         (define-key map (kbd "M-a") 'mark-whole-buffer)                 ; select all
+        (define-key map (kbd "M-b") 'helm-buffers-list)
+        (define-key map (kbd "M-o") 'helm-find-files)
+        (define-key map (kbd "M-p") 'helm-projectile)
+        (define-key map (kbd "M-l") 'helm-semantic-or-imenu)
         map)
       "my-keys-minor-mode keymap.")
 
@@ -123,6 +125,36 @@
     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
     ; (setq package-enable-at-startup nil)
     (package-initialize)
+
+    ;;; automate package installs
+    ; list the packages you want
+    (setq package-list '(company evil expand-region fireplace git-gutter helm
+                              helm-projectile org ox-rst projectile restclient
+                              tabbar undo-tree weather-metno yasnippet
+                              zenburn-theme))
+
+    ; list the repositories containing them
+    (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                             ("gnu" . "http://elpa.gnu.org/packages/")
+                             ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+    ; activate all the packages (in particular autoloads)
+    (package-initialize)
+
+    ; fetch the list of packages available
+    (unless package-archive-contents
+      (package-refresh-contents))
+
+    ; install the missing packages
+    (dolist (package package-list)
+      (unless (package-installed-p package)
+        (package-install package)))
+
+    ;;; windmove
+    (windmove-default-keybindings)
+
+    ;;; company
+    (add-hook 'after-init-hook 'global-company-mode)
 
     ;; weather-metno-el
     (require 'weather-metno)
@@ -177,9 +209,76 @@
     (require 'helm-config)
     ;;; set helm-M-x as default
     (global-set-key (kbd "M-x") 'helm-M-x)
+    (define-key evil-normal-state-map (kbd "M-o") 'helm-find-files)
+    (define-key evil-normal-state-map (kbd "M-b") 'helm-buffers-list)
 
     ;;; fireplace
     (require 'fireplace)
+
+    ;;; yasnippet
+    (setq yas-snippet-dirs
+          '("~/.emacs.d/snippets/yasnippet-snippets"            ;; foo-mode and bar-mode snippet collection
+            ))
+
+    (yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
+
+    ;;; expand-region
+    (require 'expand-region)
+    (global-set-key (kbd "C-+") 'er/expand-region)
+
+    (evil-define-key 'normal org-mode-map
+    "+" 'er/expand-region)
+
+    ;;; undo-tree
+   (global-undo-tree-mode)
+
+    ;;; ace-jump-mode
+    (add-to-list 'load-path "/Users/mbayer/.emacs.d/elpa/ace-jump-mode-2.0/")
+    (autoload
+      'ace-jump-mode
+      "ace-jump-mode"
+      "Emacs quick move minor mode"
+    t)
+    (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+    (define-key evil-normal-state-map (kbd "f") 'ace-jump-mode)
+
+    ;;; tabbar
+    (tabbar-mode 1)
+    (global-set-key (kbd "C->") 'tabbar-forward)
+    (global-set-key (kbd "C-<") 'tabbar-backward)
+
+    (setq tabbar-background-color "#7f7f7f")
+    (custom-set-faces
+     '(tabbar-default ((t (:inherit variable-pitch :background "#7f7f7f" :foreground "black" :weight bold))))
+     '(tabbar-button-highlight ((t (:inherit tabbar-default))))
+     '(tabbar-highlight ((t (:underline t))))
+     '(tabbar-selected ((t (:inherit tabbar-default :background "#000000" :underline nil))))
+     '(tabbar-separator ((t (:inherit tabbar-default :background "#7f7f7f"))))
+     '(tabbar-unselected ((t (:inherit tabbar-default :background "#7f7f7f")))))
+
+    ;; change padding of the tabs
+    ;; we also need to set separator to avoid overlapping tabs by highlighted tabs
+    (custom-set-variables
+     '(tabbar-separator (quote (0.5))))
+    ;; adding spaces
+    (defun tabbar-buffer-tab-label (tab)
+      "Return a label for TAB.
+    That is, a string used to represent it on the tab bar."
+      (let ((label  (if tabbar--buffer-show-groups
+                        (format "[%s]  " (tabbar-tab-tabset tab))
+                      (format "%s  " (tabbar-tab-value tab)))))
+        ;; Unless the tab bar auto scrolls to keep the selected tab
+        ;; visible, shorten the tab label to keep as many tabs as possible
+        ;; in the visible area of the tab bar.
+        (if tabbar-auto-scroll-flag
+            label
+          (tabbar-shorten
+           label (max 1 (/ (window-width)
+                           (length (tabbar-view
+                                    (tabbar-current-tabset)))))))))
+
+    ;;; git-gutter
+    (global-git-gutter-mode +1)
 ;;; }
 
 ;;; generated {
