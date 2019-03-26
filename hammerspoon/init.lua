b1df09a8bec40811c66f11009d66f211f6c54c52
Markus Bayer <markus.bayer@tomcom.de>
2019-03-26 15:52:06 +0100

    added hammerspoon DeeplTranslation spoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,5 @@
+--[[ DeepLTranslate
+https://www.hammerspoon.org/Spoons/DeepLTranslate.html
+--]]
+DeeplTranslate = hs.loadSpoon('DeepLTranslate')
+DeeplTranslate:bindHotkeys({translate = { { "alt", "cmd" }, "T" },})
